973a8b4f5f6cbd6ff6d246d63cc6412402b8adf4
lucidbeaming <zenphoto@gmail.com>
2017-01-28 14:44:36 -0800

    working model

--- engine.js
+++ engine.js
@@ -1,1 +1,25 @@
-console.log (place);
+console.log(place);
+
+function makenode(txtstring){
+	var node = document.createElement("LI");
+	cnvstring = document.createTextNode(txtstring);
+	node.appendChild(cnvstring);
+	return node;
+}
+
+window.onload = function() {
+
+    for (var i = 0; i < gud.length; i++) {
+        var isit = "Is it just ok? No, it's " + gud[i] + "!";
+        console.log(isit);
+        newli = makenode(isit);
+        document.getElementById("canikickit").appendChild(newli);
+    }
+
+    Object.keys(bad).forEach(function(key, index) {
+        var yepyep = "If your " + key + " are " + bad[key] + ", that's not " + gud[index];
+        console.log(yepyep);
+        document.getElementById("whatisit").appendChild("<li>" + yepyep + "</li>");
+    });
+
+};
--- params.js
+++ params.js
@@ -1,1 +1,23 @@
 place = "holder";
+
+var gud = [
+'rad',
+'dope',
+'crackin',
+'bananas',
+'ballin',
+'awesome',
+'fire',
+'bitchin',
+'dank',
+'crazy'
+]
+
+var bad = {
+	thoughts: "narrow",
+	money: "everything",
+	plans: "irresponsible",
+	words: "unaccountable",
+	people: 1,
+	motivation: "fear"
+}
